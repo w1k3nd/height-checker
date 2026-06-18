50c5e19392c4f96d9f348d0b5f7166632ccb930b
Дмитро <kasyanovdima75@gmail.com>
2026-06-18 11:27:37 +0200

    додав код

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+
+        int count = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> test = {1, 1, 4, 2, 1, 3};
+    cout << sol.heightChecker(test) << endl;
+    return 0;
+}
